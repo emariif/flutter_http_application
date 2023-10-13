5791b9ead8ad9325dbcf7c9530b0e8b1d966ba00
emariif <okbro8788@gmail.com>
2023-10-13 12:57:52 +0700

    future with model

--- lib/http_future_builder.dart
+++ lib/http_future_builder.dart
@@ -1,24 +1,32 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_http_application/models/user_with_quick.dart';
 import 'package:http/http.dart' as myHttp;
 
 class HttpFutureBuilder extends StatelessWidget {
-  // const HttpFutureBuilder({Key? key}) : super(key: key);
-  List<Map<String, dynamic>> allUser = [];
+  List<UserModel> allUser = [];
+  HttpFutureBuilder({Key? key}) : super(key: key);
 
   Future getAllUser() async {
     // await Future.delayed(Duration(seconds: 3));
     try {
-      var respone = await myHttp.get(Uri.parse('https://reqres.in/api/users'));
+      var respone =
+          await myHttp.get(Uri.parse('https://reqres.in/api/users?page=1'));
       // print(respone.body);
       List datas = (json.decode(respone.body) as Map<String, dynamic>)['data'];
-      // datas.forEach((element) {
-      //   allUser.add(element);
-      // });
-      for (var data in datas) {
-        allUser.add(data);
-      }
+      datas.forEach((element) {
+        allUser.add(UserModel.fromJson(element));
+      });
+      // for (var data in datas) {
+      //   allUser.add(UserModel(
+      //     id: data['id'],
+      //     email: data['email'],
+      //     firstName: data['firstName'],
+      //     lastName: data['lastName'],
+      //     avatar: data['avatar'],
+      //   ));
+      // }
       print(allUser);
     } catch (e) {
       print('terdapat kesalahan');
@@ -40,15 +48,22 @@ class HttpFutureBuilder extends StatelessWidget {
                   child: Text('Loading...'),
                 );
               } else {
+                if (allUser.isEmpty) {
+                  //isEmpty = 0
+                  return const Center(
+                    child: Text('Tidak ada data...'),
+                  );
+                }
                 return ListView.builder(
                   itemCount: allUser.length,
                   itemBuilder: (context, index) => ListTile(
                     leading: CircleAvatar(
                       backgroundColor: Colors.grey[300],
-                      backgroundImage: NetworkImage(allUser[index]['avatar']),
+                      backgroundImage: NetworkImage(allUser[index].avatar),
                     ),
-                    title: Text('${allUser[index]['first_name']} ${allUser[index]['last_name']}'),
-                    subtitle: Text('${allUser[index]['email']}'),
+                    title: Text(
+                        '${allUser[index].firstName} ${allUser[index].lastName}'),
+                    subtitle: Text('${allUser[index].email}'),
                   ),
                 );
               }
---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  late String name;
+  late String email;
+  late String avatar;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.avatar,
+  });
+}
